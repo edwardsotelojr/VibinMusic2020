8d79792042588c9d52a00936ce20cd0b3a2b0e50
Edward Sotelo Jr <edwardsotelojr@gmail.com>
2020-05-11 22:52:24 -0500

    May 11. Added non user home page

--- app/javascript/components/NonUserHomePage.jsx
+++ app/javascript/components/NonUserHomePage.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import { connect } from "react-redux";
+import './NonUserHomePage.css'
 
 const mapStateToProps = state => {
     return {  };
 };
 
 const Page = () => (
-    <div>
-    <p>Non user Home Page</p>
+    <div class="Non-UserHomePage" id="home" name="non-UserHomePage">
+        <div class="LeftSide">
+            <div class="TopBroadcast">
+                Top live stream
+            </div>
+            <div class="Slideshow">
+                About Vibin
+            </div>
+        </div>
+        <div class="RightSide" id="rightSide">
+            <div class="TopSongs">
+                Top songs from the past week
+            </div>
+            <div class="TopBroadcasts">
+                Top streamers from the past week.
+            </div>
+            <div class="Trending">
+                Treading songs. highest plays from the past 3 days
+            </div>
+            <div class="GenreList">
+                select genre then display top songs from the past month.
+            </div>
+        </div>
     </div>
-
 );
 
 const NonUserHomePage = connect(mapStateToProps)(Page);
